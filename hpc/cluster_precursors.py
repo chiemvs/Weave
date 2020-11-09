7760246c54c1c7d4e68a9c3266b520c5354a5250
chiemvs <chiemvs@gmail.com>
2020-11-09 19:09:45 +0100

    Working partial correlation from hpc (not tested i.c.w. cv yet, and custom pval probably still needed). HDBSCAN in cv mode required only multiple calls in the existing loop and matching the order of lag and fold dimensions upon writing into the correlation file. Handles non-cv and cv.

--- hpc/cluster_precursors.py
+++ hpc/cluster_precursors.py
@@ -5,6 +5,7 @@ Will write clustids inside the files of the patterndir
 """
 import sys
 import logging
+import itertools
 import numpy as np
 import xarray as xr
 import pandas as pd
@@ -45,6 +46,10 @@ for corrpath in corrfiles:
     logging.debug(f'{filename} has been loaded.')
     already_clustered = hasattr(ds, outvarname)
     lags = ds.coords['lag'].values.tolist() # Lag was getting decoded. This is now solved with decode_lag passing to decode_times in xarray.
+    try:
+        folds = getattr(ds, 'fold').values.tolist() # Then we have crossvalidation present
+    except AttributeError:
+        folds = [None] # Now we mimic one
     if variable in hdbscan_kwargs:
         clusterkwargs = hdbscan_kwargs[variable].to_dict() # Non-standard if present
     else:
@@ -53,10 +58,11 @@ for corrpath in corrfiles:
     if (not already_clustered): # and (filename != 'z300_nhnorm.31.corr.nc'): 
         combined = []
         attrs = {}
-        for lag in lags:
+        for lag, fold in itertools.product(lags, folds):
             cl = Clustering()
+            field = ds[invarname].sel(lag = lag) if fold is None else ds[invarname].sel(lag = lag, fold = fold)
             try:
-                cl.reshape_and_drop_obs(array = ds[invarname], mask = ~ds[invarname].sel(lag = lag).isnull(), min_samples = 700)
+                cl.reshape_and_drop_obs(array = field.expand_dims(dim = 'lag', axis = 0), mask = ~field.isnull(), min_samples = 700) # Lag dim is expanded to obey the [n_features, nsamples] format after spatial flattening, even though the lat and lon coordinates will take up the place of the features axis
                 cl.prepare_for_distance_algorithm(manipulator = Latlons, kwargs = {'to_radians':True}) # Conversion to radians because HDBSCAN uses that.
                 clusters = cl.clustering(clusterclass = HDBSCAN, kwargs = clusterkwargs)
 #            with Clustering() as cl: # This is the memory heavy precomputed DBSCAN variety
@@ -64,16 +70,21 @@ for corrpath in corrfiles:
 #                cl.call_distance_algorithm(func = haversine_worker, n_par_processes = NPROC, distmatdtype = np.float16) 
 #                clusters = cl.clustering(clusterclass = DBSCAN, kwargs = {'eps':1300, 'min_samples':2000})
                 nclusters = int(clusters.coords["nclusters"]) # nclusters returned as coordinate because this matches bahaviour of the non-DBSCAN algorithms, even though with DBSCAN it is only a dimension of length 1
-                logging.debug(f'clustered {invarname} of {filename} by spatial haversine distance with HDBSCAN for lag: {lag}, resulting nclusters: {nclusters}')
+                logging.debug(f'clustered {invarname} of {filename} by spatial haversine distance with HDBSCAN for lag: {lag}, fold: {fold}, resulting nclusters: {nclusters}')
             except MaskingError: # Happens when masking results in zero or less than the minimum samples 
                 nclusters = 0
-                clusters = xr.DataArray(np.nan, dims = cl.samplefield.dims, coords = cl.samplefield.drop_vars('lag').coords)
-                logging.debug(f'No/too little samples were present after masking {invarname} of {filename} for lag: {lag}. HDBSCAN was not called. A field with zero clusters is returned.')
-                
-            attrs.update({f'lag{lag}':f'nclusters: {nclusters}'}) 
+                clusters = xr.DataArray(np.nan, dims = cl.samplefield.dims, coords = cl.samplefield.drop_vars(['lag','fold'], errors = 'ignore').coords)
+                logging.debug(f'No/too little samples were present after masking {invarname} of {filename} for lag: {lag}. fold: {fold}, HDBSCAN was not called. A field with zero clusters is returned.')
+            if fold is None:    
+                attrs.update({f'lag{lag}':f'nclusters: {nclusters}'}) 
+            else:
+                attrs.update({f'lag{lag}_fold{fold}':f'nclusters: {nclusters}'}) 
             combined.append(clusters.squeeze().drop_vars('nclusters', errors = 'ignore'))
         
-        temp = xr.concat(combined, dim = pd.Index(lags, name = 'lag'))
+        if fold is None:
+            temp = xr.concat(combined, dim = pd.Index(lags, name = 'lag')) # Immediately at first position, and correct order
+        else:
+            temp = xr.concat(combined, dim = pd.MultiIndex.from_product([lags,folds], names = ['lag','fold'])).unstack('concat_dim').transpose(*ds[invarname].dims).reindex_like(ds[invarname]) # Unstack brings the lag/fold dimension to last place and scrambled order, so so transpose and reindex to get original ordering
         ds.close() # Need to close before writer can access
         attrs.update({key:str(item) for key,item in clusterkwargs.items()})
         w = Writer(corrpath,varname = outvarname) # Should be able to find the dataformat
--- hpc/find_precursors.py
+++ hpc/find_precursors.py
@@ -9,6 +9,7 @@ import xarray as xr
 import pandas as pd
 from pathlib import Path
 from scipy.signal import detrend
+from scipy.stats import spearmanr
 
 TMPDIR = Path(sys.argv[1])
 PACKAGEDIR = sys.argv[2] 
@@ -22,16 +23,16 @@ sys.path.append(PACKAGEDIR)
 from Weave.processing import TimeAggregator
 from Weave.association import Associator
 from Weave.inputoutput import Writer
-from Weave.utils import agg_time, spearmanr_wrap, kendall_predictand #kendall_choice, chi
+from Weave.utils import agg_time, spearmanr_par, prepare_scipy_stats_for_crossval #kendall_predictand #kendall_choice, chi
 
 logging.basicConfig(filename= TMPDIR / 'find_precursors.log', filemode='w', level=logging.DEBUG, format='%(process)d-%(relativeCreated)d-%(message)s')
 # Open a response timeseries. And extract a certain cluster with a cluster template
 response = xr.open_dataarray(ANOMDIR / 't2m_europe.anom.nc')
 clusterfield = xr.open_dataarray(CLUSTERDIR / 't2m-q095.nc').sel(nclusters = 14)
-#reduced = response.groupby(clusterfield).mean('stacked_latitude_longitude')
-#reduced = reduced.sel(clustid = 9) # In this case cluster 9 is western europe.
-reduced = response.groupby(clusterfield).quantile(q = 0.8, dim = 'stacked_latitude_longitude')
+reduced = response.groupby(clusterfield).mean('stacked_latitude_longitude')
 reduced = reduced.sel(clustid = 9) # In this case cluster 9 is western europe.
+#reduced = response.groupby(clusterfield).quantile(q = 0.8, dim = 'stacked_latitude_longitude')
+#reduced = reduced.sel(clustid = 9) # In this case cluster 9 is western europe.
 response.close()
 del response
 
@@ -45,6 +46,12 @@ files.remove('swvl3_europe.anom.nc')
 files.remove('z300_nhmin.anom.nc') # Only nhnorm retained
 to_reduce = ['snowc','siconc'] # Variables that are reduced and stacked etc, such that they are not too large for parallel association
 #files = ['sst_nhplus.anom.nc', 'z300_nhnorm.anom.nc', 'swvl13_europe.anom.nc']
+files = ['sst_nhplus.anom.nc']
+
+# Testing the cross-validation setting
+#asofunc = crossvalidate(5,True,True)(prepare_scipy_stats_for_crossval(spearmanr)) 
+# Testing the partial correlation settion
+asofunc = spearmanr_par
 
 timeaggs = [1, 3, 5, 7, 11, 15, 21, 31] # Block/rolling aggregations.
 #timeaggs = [1,7,30] # Block/rolling aggregations.
@@ -55,8 +62,10 @@ for timeagg in timeaggs:
     laglist = [0,] + list(-timeagg - absolute_separation) # Dynamic lagging to avoid overlap, lag zero is the overlap
     # Aggregate the response, subset and detrend
     responseagg = agg_time(array = reduced, ndayagg = timeagg, method = 'mean', rolling = True, firstday = pd.Timestamp('1981-01-01'))
-    summersubset = responseagg[responseagg.time.dt.season == 'JJA']
-    summersubset.values = detrend(summersubset.values)
+    response_t1 = np.column_stack([responseagg[timeagg:],responseagg[:-timeagg]]) # First column is concurrent. 2nd is value from one step back (t-1)
+    response_t1 = xr.DataArray(response_t1, dims = ('time','what'), coords = {'time':responseagg.coords['time'][timeagg:], 'what':['t0','t-1']})
+    summersubset = response_t1[response_t1.time.dt.season == 'JJA']
+    summersubset.values = detrend(summersubset.values, axis = 0)
     for inputfile in files:
         # Investigate the precursors
         name = inputfile.split('.')[0]
@@ -66,7 +75,7 @@ for timeagg in timeaggs:
             ta = TimeAggregator(datapath = ANOMDIR / inputfile, share_input = True, reduce_input = (varname in to_reduce))
             mean = ta.compute(nprocs = NPROC, ndayagg = timeagg, method = 'mean', firstday = pd.Timestamp(responseagg.time[0].values), rolling = True)
             del ta
-            ac = Associator(responseseries = summersubset, data = mean, laglist = laglist, association = spearmanr_wrap)
+            ac = Associator(responseseries = summersubset, data = mean, laglist = laglist, association = asofunc, timeagg = timeagg, is_partial = True, n_folds = None)
             del mean
             corr = ac.compute(NPROC, alpha = 5*10**(-6 - 0.2*(timeagg-1))) # Variable alpha, ranges from 5e-6 to 5e-12 for timeaggs 1 to 31
             if varname in to_reduce:
