af933d1c012780254857e10e3ed39a9fbce034ff
chiemvs <chiemvs@gmail.com>
2020-11-20 21:09:54 +0100

    HDBSCAN attempt parameters for reference

--- hpc/cluster_precursors.py
+++ hpc/cluster_precursors.py
@@ -23,17 +23,23 @@ sys.path.append(PACKAGEDIR)
 from Weave.inputoutput import Writer, Reader
 from Weave.clustering import Clustering, haversine_worker, Latlons, MaskingError
 
-logging.basicConfig(filename= TMPDIR / 'cluster_precursors.log', filemode='w', level=logging.DEBUG, format='%(process)d-%(relativeCreated)d-%(message)s')
+logging.basicConfig(filename= TMPDIR / 'cluster_precursors9_z300.log', filemode='w', level=logging.DEBUG, format='%(process)d-%(relativeCreated)d-%(message)s')
 
 # Clusterkwargs for hdbscan, coming from manual selection
-#hdbscan_kwargs = pd.DataFrame([[2000,600],[5000,2000],[True,False]], columns = ['z300','standard'], index = ['min_samples','min_cluster_size','allow_single_cluster'])
+hdbscan_kwargs = pd.DataFrame([[2000,4000,600,1000,600,1500,500],[7000,9000,3000,1500,3000,5000,1500],[True,True,True,True,True,True,True]], columns = ['t850','z300','tcc','sst','standard','snowc','siconc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt9
+#hdbscan_kwargs = pd.DataFrame([[2000,4000,600,2000,600,2000,300],[4000,5000,2000,3500,2000,5000,1500],[True,True,True,True,True,True,False]], columns = ['t850','z300','tcc','sst','standard','snowc','siconc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt8
+#hdbscan_kwargs = pd.DataFrame([[2000,4000,600,2000,600,2000,500],[5000,5000,2000,2500,2000,5000,1500],[True,True,True,False,True,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc','siconc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt7
+#hdbscan_kwargs = pd.DataFrame([[2000,4000,600,2000,600,2000,500],[5000,7000,2000,2500,2000,5000,1500],[True,True,True,False,True,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc','siconc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt6
+#hdbscan_kwargs = pd.DataFrame([[2000,4000,600,2000,600,2000],[5000,7000,2000,2500,2000,5000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt5
 #hdbscan_kwargs = pd.DataFrame([[2000,2000,600,1500,600,2000],[5000,7000,2000,2500,2000,5000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt4
-hdbscan_kwargs = pd.DataFrame([[2000,2000,600,2000,600,2000],[5000,7000,2000,2500,2000,5000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt3
+#hdbscan_kwargs = pd.DataFrame([[2000,2000,600,2000,600,2000],[5000,7000,2000,2500,2000,5000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt3
 #hdbscan_kwargs = pd.DataFrame([[2000,2000,600,1000,600,1500],[5000,7000,2000,3000,2000,4000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt2
 #hdbscan_kwargs = pd.DataFrame([[2000,2000,600,1000,600,1500],[6000,6000,2000,2000,2000,2000],[True,True,True,False,False,False]], columns = ['t850','z300','tcc','sst','standard','snowc'], index = ['min_samples','min_cluster_size','allow_single_cluster']) # attempt1
 hdbscan_kwargs.loc['metric',:] = 'haversine'
 hdbscan_kwargs.loc['core_dist_n_jobs',:] = NPROC
-corrfiles = [ f for f in PATTERNDIR.glob('*corr.nc') if f.is_file() ]
+#corrfiles = [ f for f in PATTERNDIR.glob('*corr.nc') if f.is_file() ]
+corrfiles = [ f for f in PATTERNDIR.glob('*corr.nc') if f.is_file() and not (f.name[:4] == 'z300')]
+#corrfiles = [ f for f in PATTERNDIR.glob('*corr.nc') if f.is_file() and (f.name[:4] == 'z300')]
 
 # first level loop are association metric files, so variable / timeagg combinations
 # The goal is to cluster these patterns based on distance on the globe
